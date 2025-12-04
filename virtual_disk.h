441cf6e26685bfaa8b6d0541338d727b7225b1b0
Jacob Tietz <jaetietz@gmail.com>
2025-12-04 08:53:15 -0600

    Comment

--- virtual_disk.h
+++ virtual_disk.h
@@ -6,7 +6,9 @@ struct VirtualDisk {
     int num_blocks;
     int block_size;
     std::vector<std::vector<char>> blocks;
-
+    // Represents the in-memory storage of the virtual disk as a 2D array of bytes.
+    // Each inner vector is a block, simulating how data is stored on a real disk.
+    // Constructor initializes the disk with the specified number of blocks and block size.
     VirtualDisk(int n_blocks, int b_size)
         : num_blocks(n_blocks), block_size(b_size), blocks(n_blocks, std::vector<char>(b_size, 0)) {}
 };
